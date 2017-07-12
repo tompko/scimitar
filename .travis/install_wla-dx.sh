029841b26cd94fd9bf0bc6782991f987c5eb9cd1
Chris Tompkinson <chris.tompkinson@xtxmarkets.com>
2017-07-12 07:11:20 +0100

    First attempt at a hardware test

---
+++ .travis/install_wla-dx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+CACHED_VERSION=`cat ${HOME}/bin/wla-dx.version || true`
+
+if [ "${CACHED_VERSION}" = "${WLA_DX_COMMIT}" ]; then
+  echo "Using cached wla-dx binaries"
+  exit 0
+fi
+
+curl -sSL "https://github.com/vhelin/wla-dx/archive/${WLA_DX_COMMIT}.tar.gz" | tar xzv -C "${HOME}"
+cd "${HOME}/wla-dx-${WLA_DX_COMMIT}"
+cmake .
+make
+
+mkdir -p "${HOME}/bin"
+cp binaries/wla-gb binaries/wlalink "${HOME}/bin"
+echo -n "${WLA_DX_COMMIT}" > "${HOME}/bin/wla-dx.version"
